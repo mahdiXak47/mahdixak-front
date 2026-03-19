ebf5d9ec4a04dc983b11fc96ed3ef855856ffa6c
mahdiXak47 <mahdiamcfc@gmail.com>
2026-03-20 00:39:37 +0330

    icons of bootstrap added to the project

--- src/Sidebar.jsx
+++ src/Sidebar.jsx
@@ -1,93 +1,38 @@
 import './Sidebar.css';
 
-function IconHome() {
+/** Filled card + official checklist paths from Bootstrap card-checklist (no -fill glyph in the set). */
+function IconCardChecklistFilled() {
   return (
     <svg className="sidebar__icon" viewBox="0 0 16 16" fill="currentColor" aria-hidden>
-      <path d="M8.354 1.146a.5.5 0 0 0-.708 0l-6 6A.5.5 0 0 0 1.5 7.5v7a.5.5 0 0 0 .5.5h4.5a.5.5 0 0 0 .5-.5v-4h2v4a.5.5 0 0 0 .5.5H14a.5.5 0 0 0 .5-.5v-7a.5.5 0 0 0-.146-.354L13 5.793V2.5a.5.5 0 0 0-.5-.5h-1a.5.5 0 0 0-.5.5v1.293L8.354 1.146zM2.5 14V7.707l5.5-5.5 5.5 5.5V14H10v-4a.5.5 0 0 0-.5-.5h-3a.5.5 0 0 0-.5.5v4H2.5z" />
-    </svg>
-  );
-}
-
-function IconDashboard() {
-  return (
-    <svg className="sidebar__icon" viewBox="0 0 16 16" fill="currentColor" aria-hidden>
-      <path d="M8 4a.5.5 0 0 1 .5.5V6a.5.5 0 0 1-1 0V4.5A.5.5 0 0 1 8 4zM3.732 5.732a.5.5 0 0 1 .707 0l.915.914a.5.5 0 1 1-.708.708l-.914-.915a.5.5 0 0 1 0-.707zM2 10a.5.5 0 0 1 .5-.5h1.586a.5.5 0 0 1 0 1H2.5A.5.5 0 0 1 2 10zm9.5 0a.5.5 0 0 1 .5-.5h1.5a.5.5 0 0 1 0 1H12a.5.5 0 0 1-.5-.5zm.754-4.246a.389.389 0 0 0-.527-.02L7.547 9.31a.91.91 0 1 0 1.302 1.254l5.096-5.096a.389.389 0 0 0-.029-.518z" />
-      <path
-        fillRule="evenodd"
-        d="M0 10a8 8 0 1 1 15.547 2.661c-.442 1.253-1.845 1.602-2.932 1.25C11.309 13.488 9.475 13 8 13c-1.474 0-3.31.488-4.615.911-1.087.352-2.49.003-2.932-1.25A7.988 7.988 0 0 1 0 10zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.298 12.066 6.115 11 8 11s3.702 1.065 4.923 1.958c.756.246 1.477-.055 1.68-.63A7 7 0 0 0 8 3z"
+      <rect
+        className="sidebar__icon-card-fill"
+        x="1.5"
+        y="3"
+        width="13"
+        height="10"
+        rx="1"
+        ry="1"
       />
+      <path d="M7 5.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 1 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0M7 9.5a.5.5 0 0 1 .5-.5h5a.5.5 0 0 1 0 1h-5a.5.5 0 0 1-.5-.5m-1.496-.854a.5.5 0 0 1 0 .708l-1.5 1.5a.5.5 0 0 1-.708 0l-.5-.5a.5.5 0 0 1 .708-.708l.146.147 1.146-1.147a.5.5 0 0 1 .708 0" />
     </svg>
   );
 }
 
-function IconOrders() {
-  return (
-    <svg className="sidebar__icon" viewBox="0 0 16 16" fill="currentColor" aria-hidden>
-      <path d="M0 2a2 2 0 0 1 2-2h12a2 2 0 0 1 2 2v12a2 2 0 0 1-2 2H2a2 2 0 0 1-2-2V2zm15 2h-4v3h4V4zm0 4h-4v3h4V8zm0 4h-4v3h3a1 1 0 0 0 1-1v-2zm-5 3v-3H6v3h4zm-5 0v-3H1v2a1 1 0 0 0 1 1h3zm-4-4h4V8H1v3zm0-4h4V4H1v3zm5-3v3h4V4H6zm4 4H6v3h4V8z" />
-    </svg>
-  );
-}
-
-function IconProducts() {
-  return (
-    <svg className="sidebar__icon" viewBox="0 0 16 16" fill="currentColor" aria-hidden>
-      <path d="M1 2.5A1.5 1.5 0 0 1 2.5 1h3A1.5 1.5 0 0 1 7 2.5v3A1.5 1.5 0 0 1 5.5 7h-3A1.5 1.5 0 0 1 1 5.5v-3zM2.5 2a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 1h3A1.5 1.5 0 0 1 15 2.5v3A1.5 1.5 0 0 1 13.5 7h-3A1.5 1.5 0 0 1 9 5.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zM1 10.5A1.5 1.5 0 0 1 2.5 9h3A1.5 1.5 0 0 1 7 10.5v3A1.5 1.5 0 0 1 5.5 15h-3A1.5 1.5 0 0 1 1 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3zm6.5.5A1.5 1.5 0 0 1 10.5 9h3a1.5 1.5 0 0 1 1.5 1.5v3a1.5 1.5 0 0 1-1.5 1.5h-3A1.5 1.5 0 0 1 9 13.5v-3zm1.5-.5a.5.5 0 0 0-.5.5v3a.5.5 0 0 0 .5.5h3a.5.5 0 0 0 .5-.5v-3a.5.5 0 0 0-.5-.5h-3z" />
-    </svg>
-  );
-}
-
-function IconCustomers() {
-  return (
-    <svg className="sidebar__icon" viewBox="0 0 16 16" fill="currentColor" aria-hidden>
-      <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z" />
-      <path
-        fillRule="evenodd"
-        d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-6.603 9.329c.203.575.923.876 1.68.63C4.298 12.066 6.115 11 8 11s3.702 1.065 4.923 1.958c.756.246 1.477-.055 1.68-.63A7 7 0 0 0 8 1z"
-      />
-    </svg>
-  );
-}
-
-function IconMusics() {
-  return (
-    <svg className="sidebar__icon" viewBox="0 0 16 16" fill="currentColor" aria-hidden>
-      <path d="M6 13c0 1.657-1.343 3-3 3S0 14.657 0 13s1.343-3 3-3 3 1.343 3 3z" />
-      <path d="M9.346 5.345 7.828 7.091A.5.5 0 0 0 8 7.5v5a.5.5 0 0 0 .5.5 2.5 2.5 0 0 1 0 5 .5.5 0 0 0 .5.5h1.528a2.5 2.5 0 0 0 2.443-1.973l.671-3.356a1.5 1.5 0 0 0-1.188-1.771l-1.656-.331a1.5 1.5 0 0 1-1.17-.869l-.308-.617A1.5 1.5 0 0 0 9.346 5.345z" />
-      <path d="m11.536 9.436.671 3.356a2.5 2.5 0 0 1-2.443 2.027H9.748a2.5 2.5 0 0 1-2.443-2.027l-.671-3.356a1.5 1.5 0 0 1 1.188-1.771l1.656-.331a1.5 1.5 0 0 0 1.17-.869l.308-.617A1.5 1.5 0 0 1 11.536 5.43z" />
-    </svg>
-  );
-}
-
-function IconBrand() {
-  return (
-    <svg className="sidebar__brand-svg" viewBox="0 0 16 16" fill="currentColor" aria-hidden>
-      <path d="M2.5 4A1.5 1.5 0 0 1 4 2.5h8A1.5 1.5 0 0 1 13.5 4v8a1.5 1.5 0 0 1-1.5 1.5h-8A1.5 1.5 0 0 1 2.5 12V4zm1 0v8a.5.5 0 0 0 .5.5h8a.5.5 0 0 0 .5-.5V4a.5.5 0 0 0-.5-.5h-8a.5.5 0 0 0-.5.5z" />
-      <path d="M6.5 5.5a.5.5 0 0 1 .5-.5h2a.5.5 0 0 1 .5.5v5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-5zm1 .5v4h1V6h-1z" />
-    </svg>
-  );
-}
-
-function IconChevronDown() {
-  return (
-    <svg className="sidebar__chevron" viewBox="0 0 16 16" fill="currentColor" aria-hidden>
-      <path
-        fillRule="evenodd"
-        d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"
-      />
-    </svg>
-  );
+function BiIcon({ name, className = '' }) {
+  return <i className={`bi bi-${name} sidebar__bi ${className}`.trim()} aria-hidden />;
 }
 
 const NAV_ITEMS = [
-  { id: 'home', label: 'Home', icon: IconHome, active: true },
-  { id: 'dashboard', label: 'Dashboard', icon: IconDashboard },
-  { id: 'orders', label: 'Orders', icon: IconOrders },
-  { id: 'products', label: 'Products', icon: IconProducts },
-  { id: 'customers', label: 'Customers', icon: IconCustomers },
+  { id: 'home', label: 'Home', icon: () => <BiIcon name="house-fill" />, active: true },
+  { id: 'dashboard', label: 'Dashboard', icon: () => <BiIcon name="speedometer2" /> },
+  { id: 'orders', label: 'Orders', icon: () => <BiIcon name="table" /> },
+  { id: 'products', label: 'Products', icon: () => <BiIcon name="grid-fill" /> },
+  { id: 'customers', label: 'Customers', icon: () => <BiIcon name="people-fill" /> },
+  { id: 'notes-to-do', label: 'Notes to do', icon: IconCardChecklistFilled },
   {
     id: 'musics',
     label: 'Musics',
-    icon: IconMusics,
+    icon: () => <BiIcon name="apple-music" />,
     externalHref: 'https://mahdixak-music.darkube.app/',
   },
 ];
@@ -97,7 +42,7 @@ export default function Sidebar() {
     <aside className="sidebar" aria-label="Main navigation">
       <div className="sidebar__header">
         <span className="sidebar__brand-mark" aria-hidden>
-          <IconBrand />
+          <BiIcon name="layout-sidebar-inset" className="sidebar__bi--brand" />
         </span>
         <span className="sidebar__brand-text">Sidebar</span>
       </div>
@@ -125,7 +70,7 @@ export default function Sidebar() {
             m
           </span>
           <span className="sidebar__username">mdo</span>
-          <IconChevronDown />
+          <BiIcon name="caret-down-fill" className="sidebar__bi--footer" />
         </button>
       </div>
     </aside>
